509609257ccf3190345336577ac5ab33a9be24ec
Thomas Morini <thomas.morini@icloud.com>
2025-11-12 15:44:39 +0100

    modification URL fetch

--- components/Home.js
+++ components/Home.js
@@ -11,7 +11,7 @@ function Home() {
   const [moviesData, setMoviesData] = useState([]);
 
   useEffect(() => {
-   fetch('http://localhost:3000/movies')
+   fetch('https://mymoviz-back-gamma.vercel.app/movies')
      .then(response => response.json())
      .then(data => {
        setMoviesData(data.movies);
